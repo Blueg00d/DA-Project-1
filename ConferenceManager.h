cc3726a1ad4b0f8de6dde6b3d9e8e377e70cc37a
Blueg00d <reisg5010@gmail.com>
2026-03-18 21:18:03 +0000

    Enhanced ConferenceManager Class with new attributes/methods

--- ConferenceManager.h
+++ ConferenceManager.h
@@ -2,20 +2,40 @@
 #define DA_PROJECT_1_CONFERENCEMANAGER_H
 #include "Reviewer.h"
 #include "Submission.h"
+#include "Graph.h"
 #include <string>
 #include <unordered_map>
 using namespace std;
 
 class ConferenceManager {
     private:
+    //Private Attributes
     unordered_map<int, Reviewer> reviewers;
     unordered_map<int, Submission> submissions;
     int minReviewsPerSubmission;
     int maxReviewsPerReviewer; // Acho que fica melhor assim, sara vê pls
+    int generateAssignment;
+    int riskAnalysis;
 
+    Graph<int> graph;
+
+    //Private Methods
     void createNodes();
+    void connectSourceSinkToNodes();
+    void connectNodes();
+    
 
     public:
+    // Constructor
+    ConferenceManager(
+        unordered_map<int, Reviewer> reviewers,
+        unordered_map<int, Submission> submissions,
+        int minReviewsPerSubmission,
+        int maxReviewsPerReviewer,
+        int generateAssignement,
+        int riskAnalysis
+    );
+
     bool loadFromCSV(string filename);
     void buildGraph();
     void runAssignment();
