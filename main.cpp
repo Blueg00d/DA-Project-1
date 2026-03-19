6b97b3e3928f21289d61a8045d7ed87805b8bb59
sarovski <sara.f.babau@gmail.com>
2026-03-19 16:13:27 +0000

    small changes on main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,33 @@ using namespace std;
 
 void handleChoice(int choice, ConferenceManager &conferenceManager) {
     switch (choice) {
-        //TODO
+        case 1: {
+            //input file
+            // string filename;
+            // cout << "insert the name of your file: ";
+            // cin << filename;
+            // função de parser da rita
+        }
     }
 }
+
 int main() {
     ConferenceManager manager;
     int choice;
 
     while (true) {
         cout << "☆ Choose which path to follow (this action will have consequences):" << endl;
-        cout << "1: " << endl;
+        cout << "1: Test input" << endl;
         cout << "2: " << endl;
         cout << "etc" << endl;
         cout << "0: sair :p" << endl;
         cout << "input: ";
         cin >> choice;
 
-        if (choice == 0) break;
+        if (choice == 0) {
+            cout << "leaving ! :p" << endl;
+            break;
+        }
         handleChoice(choice, manager);
     }
     return 0;
